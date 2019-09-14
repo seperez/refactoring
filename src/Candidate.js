8ffd5928bd4c85b554f958bacb5f88bf0818c440
Sebastián M. Pérez <perezsebastianm@gmail.com>
2019-09-14 10:49:10 -0300

    Remove unnecesary class

--- src/Candidate.js
+++
@@ -1,9 +0,0 @@
-class Candidate {
-  constructor(name, latitude, longitude) {
-    this.name = name;
-    this.latitude = latitude;
-    this.longitude = longitude;
-  }
-}
-
-export default Candidate;
